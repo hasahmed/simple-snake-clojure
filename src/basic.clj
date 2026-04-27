01f834b08f9ea36793d8a6175f42714289a5ad0e
Hasan Ahmed <hasahmed@ironarchtech.com>
2026-04-27 04:58:25 -0400

    First commit

---
+++ src/basic.clj
@@ -0,0 +1,24 @@
+(ns circle.core
+  (:import [javax.swing JFrame JPanel]
+           [java.awt Color Graphics Dimension]))
+
+(defn make-panel []
+  (proxy [JPanel] []
+    (paintComponent [^Graphics g]
+      (proxy-super paintComponent g)
+      (.setColor g Color/BLACK)
+      (.fillOval g 150 100 200 200))
+    (.setColor g Color/RED)
+    (.fillOval g 150 100 200 200))
+  (getPreferredSize []
+                    (Dimension. 500 400))))
+
+(defn -main [& _args]
+  (let [frame (JFrame. "Black Circle")
+        panel (make-panel)]
+    (.setBackground panel Color/WHITE)
+    (.add (.getContentPane frame) panel)
+    (.pack frame)
+    (.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
+    (.setLocationRelativeTo frame nil)
+    (.setVisible frame true)))
