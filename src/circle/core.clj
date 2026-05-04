3fefff2c855df0294c5d1afd4a3dc2558a8705dc
Hasan Ahmed <hasahmed@ironarchtech.com>
2026-05-04 00:14:53 -0400

    Stuff is going well

--- src/circle/core.clj
+++ src/circle/core.clj
@@ -1,6 +1,6 @@
 (ns circle.core
-  (:import (java.awt.event WindowEvent)
-           [javax.swing JFrame JPanel]
+  (:import (java.awt.event ActionListener KeyListener)
+           [javax.swing JFrame JPanel Timer]
            [java.awt Color Graphics Dimension]))
 
 (defn make-panel []
@@ -20,6 +20,24 @@
     (.pack frame)
     (print "Hello")
     (.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
+    (.start (Timer. 75 (reify ActionListener
+                 (actionPerformed [this actionEvent]
+                   ;(println "Goodbye")
+                   ;(println this)
+                   ;(println actionEvent)
+                 ))))
+    (.addKeyListener frame (reify KeyListener
+                       (keyPressed [this keyEvent]
+                         (println keyEvent)
+                         )
+                       (keyReleased [this keyEvent]
+                         ;(println keyEvent)
+                         )
+                       (keyTyped [this keyEvent]
+                         :default
+                         ;(println keyEvent)
+                         )
+                             ))
     ;(.dispatchEvent frame (WindowEvent. frame WindowEvent/WINDOW_CLOSING))
 
     (.setLocationRelativeTo frame nil)
